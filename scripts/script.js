2087b97b2fd251de8ffdbe56360f30d43246eb8f
Jett Gentes <146028226+SUPERDERP1@users.noreply.github.com>
2024-12-13 14:29:46 -0800

    not fully done but commiting anyways

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
 // i hvvae no idea what im doing
+const inputReader = document.getElementById('inputReader');
+const cmds = [];
+inputReader.addEventListener("keydown", (event) => {
+
+    if (event.key === 'ENTER') {
+        if (cmds.includes("return")){
+
+        }else if(cmds.includes("console.log()"))
+    }
+
+})
