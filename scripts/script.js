29e978aa578a78f633828042a4dd953c462ce0e8
Jett Gentes <146028226+SUPERDERP1@users.noreply.github.com>
2024-12-13 18:01:09 -0800

    commitCode

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,23 @@
 // i hvvae no idea what im doing
 const inputReader = document.getElementById('inputReader');
 const cmds = [];
+let consoleLogUnlocked = 0;
+let semicolons = 0;
 inputReader.addEventListener("keydown", (event) => {
 
     if (event.key === 'ENTER') {
-        if (cmds.includes("return")){
+        if (cmds.includes("return;")){
 
-        }else if(cmds.includes("console.log()"))
+        }else if(cmds.includes("console.log();")){
+
+        }else if(cmds.includes("let semicolons = 0;")){
+
+        }else if(cmds.includes("semicolons += 1")){
+
+        }else if(cmds.includes("var"))
+
+        }else if(cmds.includes("if(money < 100000) {givemoney();}"))
     }
 
 })
+var setvar
