a9d1b748969c1cd6797b066d247539510e00331d
Jett Gentes <146028226+SUPERDERP1@users.noreply.github.com>
2024-12-15 21:30:48 -0800

    WORK PLEASE

--- scripts/script.js
+++ scripts/script.js
@@ -82,7 +82,7 @@ function processCommandShop(command) {
     const cost = allCmds[cmdText];
 
     if (semicolons >= cost) {
-        semicolons -= cost;
+        semicolons - cost;
         cmds.push(cmdText); // Add to available commands
         const shopIndex = shopCmds.indexOf(cmdText);
         shopCmds.splice(shopIndex, 1); // Remove from shop
